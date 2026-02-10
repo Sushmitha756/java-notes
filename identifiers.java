869f8403d6c188aa3472c7889f657e64cc630317
Sushmitha G <sushmithagsushmithag788@gmail.com>
2026-02-10 16:24:42 +0530

    Commited

---
+++ identifiers.java
@@ -0,0 +1,11 @@
+class identifiers {
+    static int _cloud23 = 45;
+    //"static int 12cloud = 34;"//it can not start with numbers
+    static int a = 10;
+    static float b = 50f;
+    static double c = 20;
+    static char d = 's';
+    public static void main(String[] args) {
+        System.out.println(d);
+    }
+}
---
+++ operators.java
@@ -0,0 +1,30 @@
+class operators {
+    static int a = 10 ;
+    static int b = 90;
+    static int c = 244;
+    public static void main(String[] args) {
+        //System.err.println("addition of a and b=" +(a+b));//40
+        //System.out.println("values of a and b =" +a+b);
+        //System.out.println("post increment of a =" +(a++));//10
+       // System.out.println(a);//11
+       // System.out.println("pre increment of a =" +(++a));//12
+       // System.out.println("predecrement of a=" +(--a));//11
+       // System.out.println("post decrement of a="+(a--));//11
+       // System.out.println(a);//10
+       //bitwise operator
+       //System.out.println(a&b);
+       //System.out.println(a|b);
+       //System.out.println(a&b|c);
+       //System.out.println(a<<2);
+       //System.out.println(a>>2);
+       //logical
+       System.out.println(a>b && a<b || a<b);
+       //assignment operator
+       //a += 2;
+      // System.out.println(a);
+     
+
+    }
+   
+
+}
