36454dadd2b3176db6935bfaf56a12add37216c2
Sushmitha G <sushmithagsushmithag788@gmail.com>
2026-02-11 16:19:52 +0530

    commited

---
+++ controlstatements.java
@@ -0,0 +1,69 @@
+class controlstatements {
+    public static void main(String[] args) {
+
+        // if(true){
+        //     System.out.println("this condition is true");
+        // }
+        // System.out.println("none of the condition");
+//if else condition
+       // int age = 15;
+        // if(age >= 18){
+        //     System.out.println("Eligible to vote");
+        // }
+        // else {
+        //     System.out.println("not eligible to vote");
+        // }
+        //if else if condition
+    //  int a = 30;
+    //     if (a >= 2 && a < 15){
+    //         System.out.println("It works");
+    //     }
+    //     else if ((a >= 15) && (a < 25)){
+    //         System.out.println("It does not work");
+    //     }
+    //     else if (a >= 25 && a < 30){
+    //         System.out.println("It works");
+    //     }
+    //     else if (a <= 31){
+    //         System.out.println("It works");
+    //     }
+    //     else {
+    //         System.out.println("None of the condition is true");
+    //     }
+    
+// int sub1 = 89;
+// int sub2 = 90;
+// if (sub1 == sub2){
+//     System.out.println("two subs have same marks");
+// }
+// else if(sub1 > sub2){
+//     System.out.println("sub1 is greater than sub2");
+// }
+// else if (sub1 < sub2){
+//     System.out.println("sub2 is greater than sub1");
+// }
+// else {
+//     System.out.println("have separate marks");
+// }
+
+   int marks = 90;
+   System.out.println("name:Sushma");
+   if(marks >= 85){
+    System.out.println("grade: A+");
+   }
+   else if(marks >= 75 && marks < 85){
+    System.out.println("A");
+   }
+   else if(marks >= 60 && marks < 75){
+    System.out.println("B+");
+   }
+   else if(marks >= 55 && marks < 60){
+    System.out.println("B");
+   }
+   else{
+    System.out.println("FAIL");
+   }
+   
+    }
+    
+}
